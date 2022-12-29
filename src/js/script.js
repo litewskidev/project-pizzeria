931837376d703f87d135cc969b2d9d1981c45eba
Adrian Litewski <litewskidev@gmail.com>
2022-12-29 23:39:18 +0100

    calculate price for options

--- src/js/script.js
+++ src/js/script.js
@@ -60,7 +60,10 @@
       thisProduct.data = data;
 
       thisProduct.renderInMenu();
+      thisProduct.getElements();
       thisProduct.initAccordion();
+      thisProduct.initOrderForm();
+      thisProduct.processOrder();
 
       console.log('new Product:', thisProduct);
     }
@@ -78,14 +81,21 @@
       menuContainer.appendChild(thisProduct.element);
     }
 
+    getElements(){
+      const thisProduct = this;
+
+      thisProduct.accordionTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      thisProduct.form = thisProduct.element.querySelector(select.menuProduct.form);
+      thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
+      thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
+      thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+    }
+
     initAccordion(){
       const thisProduct = this;
-      
-      /* [DONE] find the clickable trigger (the element that should react to clicking) */
-      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
 
       /* [DONE] START: add event listener to clickable trigger on event click */
-      clickableTrigger.addEventListener('click', function(event) {
+      thisProduct.accordionTrigger.addEventListener('click', function(event) {
         /* [DONE] prevent default action for event */
         event.preventDefault();
         /* [DONE] find active product (product that has active class) */
@@ -96,6 +106,70 @@
         }
         /* [DONE] toggle active class on thisProduct.element */
         thisProduct.element.classList.toggle('active');
+      });
+    }
+
+    initOrderForm(){
+      //const thisProduct = this;
+
+      console.log(this.initOrderForm);
+    }
+
+    processOrder(){
+      const thisProduct = this;
+
+      /* [DONE] covert form to object structure e.g. { sauce: ['tomato'], toppings: ['olives', 'redPeppers']} */
+      const formData = utils.serializeFormToObject(thisProduct.form);
+      console.log('formData', formData);
+
+      /* [DONE] set price to default price */
+      let price = thisProduct.data.price;
+
+      /* [DONE] for every category (param)... */
+      for(let paramId in thisProduct.data.params) {
+        /* [DONE] determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... } */
+        const param = thisProduct.data.params[paramId];
+        console.log(paramId, param);
+
+        /* [DONE] for every option in this category */
+        for(let optionId in param.options) {
+          /* [DONE] determine option value */
+          const option = param.options[optionId];
+          console.log(optionId, option);
+
+          /* [DONE] check if there is param with a name of paramId in formData and if it includes optionId */
+          if(formData[paramId] && formData[paramId].includes(optionId)) {
+            /* [DONE] check if the option is not default */
+            if(!option.default) {
+              /* [DONE] add option price to price variable */
+              price += option.price;
+            }
+          } else {
+            /* [DONE] check if the option is default */
+            if(option.default) {
+              /* [DONE] reduce price variable */
+              price -= option.price;
+            }
+          }
+        }
+      }
+      /* [DONE] update calculated price in the HTML */
+      thisProduct.priceElem.innerHTML = price;
+
+      thisProduct.form.addEventListener('submit', function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+
+      for(let input of thisProduct.formInputs){
+        input.addEventListener('change', function(){
+          thisProduct.processOrder();
+        });
+      }
+
+      thisProduct.cartButton.addEventListener('click', function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
       });
     }
   }
