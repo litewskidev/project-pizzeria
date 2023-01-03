0c9bade262ebc3bbd2846f77a88d6fadb1c4b90e
Adrian Litewski <litewskidev@gmail.com>
2023-01-03 23:50:19 +0100

    implement API

--- src/js/script.js
+++ src/js/script.js
@@ -535,14 +535,30 @@
       //console.log('thisApp.data', thisApp.data);
 
       for(let productData in thisApp.data.products){
-        new Product(productData, thisApp.data.products[productData]);
+        new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]);
       }
     },
 
     initData: function(){
       const thisApp = this;
 
-      thisApp.data = dataSource;
+      thisApp.data = {};
+
+      const url = settings.db.url + '/' + settings.db.products;
+      fetch(url)
+        .then(function(rawResponse){
+          return rawResponse.json();
+        })
+        .then(function(parsedResponse){
+          console.log('parsedResponse', parsedResponse);
+
+          /* save parsedResponse as thisApp.data.products */
+          thisApp.data.products = parsedResponse;
+          /* execute initMenu method */
+          thisApp.initMenu();
+        });
+
+      console.log('thisApp.data', JSON.stringify(thisApp.data));
     },
 
     initCart: function(){
@@ -561,7 +577,6 @@
       console.log('templates:', templates);*/
 
       thisApp.initData();
-      thisApp.initMenu();
       thisApp.initCart();
     },
   };
