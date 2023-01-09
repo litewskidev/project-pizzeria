01b11a112b8e0b44ad08c58d12a06489054b4fc4
Adrian Litewski <litewskidev@gmail.com>
2023-01-09 05:01:34 +0100

    add home page structure

--- dist/js/app.js
+++ dist/js/app.js
@@ -2,6 +2,7 @@ import {settings, select, classNames} from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
 import Booking from './components/Booking.js';
+import Home from './components/Home.js';
 
 const app = {
   initPages: function(){
@@ -113,6 +114,12 @@ const app = {
     thisApp.booking = new Booking(bookingContainer);
   },
 
+  initHome: function(){
+    const thisApp = this;
+    const homeContainer = document.querySelector(select.containerOf.home);
+    thisApp.home = new Home(homeContainer);
+  },
+
   init: function(){
     const thisApp = this;
 
@@ -120,6 +127,7 @@ const app = {
     thisApp.initData();
     thisApp.initCart();
     thisApp.initBooking();
+    thisApp.initHome();
   },
 };
 
